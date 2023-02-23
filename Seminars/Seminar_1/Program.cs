4cd18ba01125c1653d1df24f005a644706561b5f
KsenofontovPavel <kpo89@inbox.ru>
2023-02-23 22:18:32 +0300

    Add Seminar1

---
+++ Seminars/Seminar_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Команда, которая выводит что-то в консоль
+// Console.WriteLine("Hello, World 123!");
+// Получение входного числа от пользователя
+// Console.Write("Введи целое число: ");
+// int number = int.Parse(Console.ReadLine());
+// Квадрат входного числа
+//int result = number * number;
+// Console.WriteLine($"Квадрат входного числа = {result}");
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int result = number % 10;
+
+Console.WriteLine($"Последняя цифра = {result}");
