5d67d6b0473e7b064638418575b1e6157449fade
KsenofontovPavel <kpo89@inbox.ru>
2023-02-27 22:35:03 +0300

    Add Seminar_2

---
+++ Seminars/Seminar_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine($"Случайное число: {number}");
+int firstDigit = number / 10; 
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
+ {
+    Console.WriteLine(firstDigit );
+ }
+ else
+ {
+    Console.WriteLine(secondDigit);
+ }
