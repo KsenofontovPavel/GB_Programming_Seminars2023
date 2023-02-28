aa2b33c6f8db50fc2fc1ab4b83213510039d9220
KsenofontovPavel <kpo89@inbox.ru>
2023-02-28 21:31:23 +0300

    Add seminar_2 TASK10

---
+++ Seminars/Homework_seminar _2 _TASK_10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую цифру этого
+числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int Length = number.ToString().Length;
+if (Length == 3) 
+{
+	int result = ( number/10 ) % 10;
+	Console.WriteLine("Вторая цифра этого числа " + result);
+} 
+else 
+{
+	Console.WriteLine("Число не является трёхзначным");
+}
