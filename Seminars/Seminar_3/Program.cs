198b57d83c67b04e8554cc55cc98c7bc12efbc13
KsenofontovPavel <kpo89@inbox.ru>
2023-03-02 22:44:43 +0300

    Add Seminar_3

---
+++ Seminars/Seminar_3/Program.cs
@@ -0,0 +1,91 @@
+﻿/*// while
+int count = 0;
+while (count < 100)
+{
+    Console.WriteLine($"{count}.Hello world");
+    count++;
+}
+
+Console.WriteLine("Выход из цикла while");*/
+
+/*// for
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"{i}.Hello world (for)");
+}
+
+Console.WriteLine("Выход из цикла for");*/
+
+
+/*Console.WriteLine("Hello world");
+int number = Math.Min(10, 15);
+Console.WriteLine(number);*/
+
+/* [возвращаемый тип] [имя метода] [(параметров)] {} */
+
+/*int GetSum(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+int sum = GetSum (10, 15);
+Console.WriteLine(sum);*/
+
+/*int FindCoordinates(int arg)
+ 
+{
+ if (arg == 1 )
+ {
+    Console.Write("x = (0 --> ∞) : y = (0 --> ∞)");
+ }
+
+if (arg == 2 )
+{
+    Console.Write("x = (0 --> -∞) : y = (0 --> ∞)");
+}
+if (arg == 3 )
+{
+    Console.Write("x = (0 --> -∞) : y = (0 --> -∞)");
+}
+if (arg == 3 )
+{
+    Console.Write("x = (0 --> ∞) : y = (0 --> -∞)");
+}
+return 0;
+}
+ Console.WriteLine("Введите ноиер четверти ");
+ int number_1_4 = int.Parse(Console.ReadLine());
+
+ FindCoordinates(number_1_4);*/
+
+
+/*double Coordinate2DSearch(double arg1_1, double arg1_2, double arg2_1, double arg2_2)
+{
+double ans = Math.Sqrt( Math.Pow((arg1_1 - arg2_1), 2) + Math.Pow((arg1_2 - arg2_2), 2));
+Console.Write("расстояние между точками в 2D пространстве : ");
+Console.WriteLine(ans);
+return 0;
+}
+
+
+Console.WriteLine("Введите координату А1 ");
+double A1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату А2 ");
+double A2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату B1 ");
+double B1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату B2 ");
+double B2 = double.Parse(Console.ReadLine());
+
+Coordinate2DSearch(A1,A2,B1,B2);*/
+
+
+
+
+
+
+
+
+
+
+
