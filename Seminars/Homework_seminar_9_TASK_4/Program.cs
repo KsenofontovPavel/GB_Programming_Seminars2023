fcca023696cf241a13b58ec1e39456a8aee49444
KsenofontovPavel <kpo89@inbox.ru>
2023-03-25 12:25:27 +0300

    Add task 1,2,3,4 seminars_9

---
+++ Seminars/Homework_seminar_9_TASK_4/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача со звездочкой Найти второй максимум в массиве.
+4, 4, 3, 0, 2 => 3 */
+
+
+
+int[] MakeArray(int len = 5, int max = 5)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("["+$"{String.Join(", ", array)}"+"]");
+}
+
+int FindSecondMax(int[] array)
+{
+    int max = 0;
+    int secondMax = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            secondMax = max;
+            max = array[i];
+        }
+        else
+        {
+            if (array[i] > secondMax && array[i] < max) secondMax = array[i];
+        }
+    }
+    
+    return secondMax;
+}
+
+int[] array = MakeArray(5, 5);
+PrintArray(array);
+Console.WriteLine(FindSecondMax(array));
